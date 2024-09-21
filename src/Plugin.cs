9f582d742e51a6ec11c26e49f81f1a68a43faa92
alduris <152772325+alduris@users.noreply.github.com>
2024-09-20 20:11:38 -0400

    I have no clue what I'm doing

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,6 +1,13 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using CharacterAndBikeEditor;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Security.Permissions;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 
 // Allows access to private members
 #pragma warning disable CS0618
@@ -13,9 +20,48 @@ namespace TestMod;
 sealed class Plugin : BaseUnityPlugin
 {
     public static new ManualLogSource Logger;
+    private static List<IDetour> Hooks = [];
 
     public void OnEnable()
     {
         Logger = base.Logger;
+
+        // Get down and dirty with the tabs
+        Hooks.Add(new Hook(typeof(CharacterEditor).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance), CharacterEditor_Start));
+    }
+
+    public void OnDisable()
+    {
+        foreach (var hook in Hooks)
+        {
+            hook.Undo();
+            hook.Dispose();
+        }
+        Hooks.Clear();
+    }
+
+    private void CharacterEditor_Start(Action<CharacterEditor> orig, CharacterEditor self)
+    {
+        orig(self);
+
+        // Add our new tab
+        Array.Resize(ref self.editorTabs, self.editorTabs.Length + 1);
+        self.editorTabs[^1] = new TestTab(self, self.editorTabs.Length - 1);
+        self.SwitchTab((CharacterEditor.EditorTab.TabID)(self.editorTabs.Length - 1), isInit: true);
+    }
+
+    public class TestTab : CharacterEditor.EditorTab
+    {
+        public TestTab(CharacterEditor owner, int id) : base(owner, (TabID)id)
+        {
+            var testButton = owner.AddComponent<Button>();
+            testButton.AddComponent<Text>().text = "Hello";
+            testButton.onClick.AddListener(owner.BackToMenu);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+        }
     }
 }
